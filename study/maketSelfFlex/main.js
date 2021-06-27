780d8db9b9845b4343432fe9870735c697a61da7
ShilkoYuliana <rtrunin@gmail.com>
2021-06-27 20:54:54 +0300

    js add

---
+++ study/maketSelfFlex/main.js
@@ -0,0 +1,62 @@
+$outer_container=$("#outer_container");
+$thumbScroller=$("#thumbScroller");
+$thumbScroller_container=$("#thumbScroller .container1");
+$thumbScroller_content=$("#thumbScroller .content");
+$thumbScroller_thumb=$("#thumbScroller .thumb");
+ 
+var sliderWidth=$thumbScroller.width();
+var itemWidth=$thumbScroller_content.width();
+ 
+$thumbScroller_content.each(function (i) {
+    totalContent=i*itemWidth;   
+    $thumbScroller_container.css("width",totalContent+itemWidth);
+});
+ 
+$thumbScroller.mousemove(function(e){
+    var mouseCoords=(e.pageX - this.offsetLeft);
+      var mousePercentY=mouseCoords/sliderWidth;
+      var destY=-(((totalContent-(sliderWidth-itemWidth))-sliderWidth)*(mousePercentY));
+      var thePosA=mouseCoords-destY;
+      var thePosB=destY-mouseCoords;
+      if(mouseCoords==destY){
+        $thumbScroller_container.stop();
+      }
+      if(mouseCoords>destY){
+        $thumbScroller_container.css("left",-thePosA);
+      }
+      if(mouseCoords<destY){
+        $thumbScroller_container.css("left",thePosB);
+      }
+});
+ 
+var fadeSpeed=300;
+ 
+$thumbScroller_thumb.each(function () {
+    var $this=$(this);
+    $this.fadeTo(fadeSpeed, 0.5);
+});
+ 
+$thumbScroller_thumb.hover(
+    function(){ //mouse over
+        var $this=$(this);
+        $this.stop().fadeTo(fadeSpeed, 1);
+    },
+    function(){ //mouse out
+        var $this=$(this);
+        $this.stop().fadeTo(fadeSpeed, 0.5);
+    }
+);
+
+
+let content = document.getElementById("present1")
+let show = document.getElementById("showContent")
+
+show.addEventListener("click", () => {
+  if (content.style.display === "block") {
+    content.style.display = "none"
+    show.value = 'Показать еще'
+  } else {
+    content.style.display = "block"
+    show.value = 'Скрыть'
+  }
+})
