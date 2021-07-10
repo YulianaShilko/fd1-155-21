f36cb0842042b4e949e21d16d6fe6ef5bc42ee8b
ShilkoYuliana <rtrunin@gmail.com>
2021-07-10 14:38:16 +0300

    full

--- study/maketSelfFlex/sortGoods.js
+++ study/maketSelfFlex/sortGoods.js
@@ -189,5 +189,15 @@ function render(arr){
 
     app.innerHTML = strHtml;
 
-    
+    $(document).ready(function(){
+ $.fn.fade_CardIn = function (ops) {
+  var $elem = this;
+  var res = $.extend({ delay: 400, speed: 400 }, ops);
+  for (var i=0, pause=0, l=$elem.length; i<l; i++, pause+=res.delay) {
+   $elem.eq(i).delay(pause).fadeIn(res.speed);
+  }
+  return $elem;
+ };
+ $('.card').fade_CardIn();
+});
 }
